8353fed0b45957c6b4d81fd0bedd8c31e6b7a47d
Marco Borg <marcob14@gmail.com>
2015-09-08 02:41:59 +0200

    * Added shortcuts support [NOTE: need to go through the code and cleanup] * Added: listener to shortcuts commands [NOTE: on MAC when background page is inactive, shortcut is not working (app is not being launched)]

--- background.js
+++ background.js
@@ -12,4 +12,26 @@ chrome.app.runtime.onLaunched.addListener(function() {
       height: 600
     }
   });
+  
+  chrome.commands.onCommand.addListener(function(command) {
+    console.log('Command:', command);
+    switch(command) {
+      case "capture":
+        
+        chrome.app.window.create('index.html', {
+          id: "CaptShare",
+          bounds: {
+            width: 700,
+            height: 600
+          }
+        });
+        capture();
+        
+        // can load a transparent window - load the js needed - and close once done?..
+        // this can also solve the desktopCapture window to be shown on top
+        break;
+      default:
+        return;
+    }
+  });
 });
---
+++ shortcutsHandler.js
@@ -0,0 +1,73 @@
+function capture() {
+  //chrome.desktopCapture.chooseDesktopMedia(["screen", "window"], captureApproved);
+  chrome.desktopCapture.chooseDesktopMedia(['screen', 'window'], null, captureApproved);
+}
+
+function captureApproved(desktop_id) {
+    if (!desktop_id) {
+        console.log('Desktop Capture access rejected.');
+        return;
+    }
+    
+    console.log("Desktop sharing started.. desktop_id:" + desktop_id);
+
+    navigator.webkitGetUserMedia({
+      audio: false,
+      video: {
+        mandatory: {
+            chromeMediaSource: 'desktop',
+            chromeMediaSourceId: desktop_id,
+            maxWidth: 4000,
+            maxHeight:4000 //setting max width & height for better image quality
+        }
+      }
+    }, gotStream, getUserMediaError);
+
+    function gotStream(stream) {
+        console.log("Received local stream");
+        var video = document.createElement('video');
+        video.addEventListener('loadedmetadata',function(){
+            //document.getElementById('btnScreenshot').style.display = 'block';
+            takeScreenshot(video, stream);
+        },false);
+        video.src = URL.createObjectURL(stream);
+        video.play();
+    }
+
+    function getUserMediaError(e) {
+      console.log('getUserMediaError: ' + JSON.stringify(e, null, '---'));
+    }
+}
+
+function takeScreenshot(video, stream) {
+  var canvas = document.createElement('canvas');
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(video, 0, 0);
+  
+  // //add watermark
+  // var watermark = document.getElementById("watermark");
+  // //changing alpha setting just for the watermark
+  // ctx.globalAlpha = 0.5;
+  // ctx.drawImage(watermark, canvas.width - 70, canvas.height - 70, 60, 60);
+  // ctx.globalAlpha = 1.0;
+  
+  //generating image url
+  var url = canvas.toDataURL();
+  console.log(url);
+  
+  //open the image in a new tab
+  window.open(url);
+  
+  var a = document.createElement('a');
+  var d = new Date();
+  a.download = 'CaptShare_' + Date.UTC(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDay());
+  a.href = url;
+  a.textContent = 'Click here to download!';
+  a.dataset.downloadurl = ['jpg', a.download, a.href].join(':');
+  a.click();
+  
+  console.log('stopping stream ', stream);
+  stream.getTracks()[0].stop();
+}
